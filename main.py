4f67d405f06e263cd3faa902fc9faea159888270
Daniel Fürst <daniel@fuerst.priv.at>
2025-05-15 19:57:39 +0200

    Fixed display on x-axis: 0 - 5 tau are clearly stated now

--- main.py
+++ main.py
@@ -10,34 +10,35 @@ if __name__ == "__main__":
     # ============== RC CONFIGURATION ==============
 
     # VOLTAGE in Volt
-    voltage = 100
+    voltage = 10
     # RESISTANCE in Ohms
-    resistance = 1000
+    resistance = 10
     # CAPACITY in Farad
-    capacity = 0.000001
+    capacity = 0.007
 
     # ============== ================ ==============
 
     tau = resistance * capacity
+    tauProcessed = (1 / tau) * tau
     current = voltage / resistance
 
     # generate time values
-    x = np.linspace(0, 5 * tau, 1000)
+    x = np.linspace(0, 5 * tauProcessed, 1000)
 
     voltageValues = []
     for time in x:
-        voltageValues.append(voltage * (1 - math.exp(-(time/tau))))
+        voltageValues.append(voltage * (1 - math.exp(-(time/tauProcessed))))
 
     currentValues = []
     for time in x:
-        currentValues.append(current * math.exp(-(time/tau)))
+        currentValues.append(current * math.exp(-(time/tauProcessed)))
 
     # subplots
     fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(10, 6))
 
     # plot Voltage
     ax1.plot(x, voltageValues, color='blue', label="Voltage (V)")
-    ax1.set_xlabel(f"τ * {tau}")
+    ax1.set_xlabel(f"τ  ({tau}s)")
     ax1.set_ylabel("uc(t) (V)")
     ax1.set_title("RC Circuit Behavior")
     ax1.grid(True)
@@ -45,7 +46,7 @@ if __name__ == "__main__":
 
     # plot Current
     ax2.plot(x, currentValues, color='red', label=f"Current (A)")
-    ax2.set_xlabel(f"τ * {tau}")
+    ax2.set_xlabel(f"τ ({tau}s)")
     ax2.set_ylabel(f"i(t) (A)")
     ax2.grid(True)
     ax2.legend()
